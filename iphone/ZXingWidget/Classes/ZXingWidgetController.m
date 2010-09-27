4980a656db4595912e501e037e7d8738397389fc
Virgilio Favero Neto <virgiliofavero@gmail.com>
2010-09-28 01:03:04 +1000

    Start capture session after adding preview layer.

--- iphone/ZXingWidget/Classes/ZXingWidgetController.m
+++ iphone/ZXingWidget/Classes/ZXingWidgetController.m
@@ -344,15 +344,15 @@
            object:self.captureSession];
 */
 
-  [self.captureSession startRunning];
-
   if (!self.prevLayer) {
     self.prevLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.captureSession];
   }
   // NSLog(@"prev %p %@", self.prevLayer, self.prevLayer);
   self.prevLayer.frame = self.view.bounds;
   self.prevLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-  [self.view.layer addSublayer: self.prevLayer];
+	[self.view.layer addSublayer: self.prevLayer];
+	
+	[self.captureSession startRunning];
 #endif
 }
 
